56269b50859c8da7d2f9dd439e2f42c5017c7b51
Jackie Bradnan <jmcgee@microsoft.com>
2018-01-29 14:42:38 -0800

    adding check for add to deck, fixed bug in failure check for first part of test

--- projects/bradnanj/dominion/unittest1.c
+++ projects/bradnanj/dominion/unittest1.c
@@ -25,7 +25,7 @@ int main() {
 	int supplyPos=0;
     int p, r, handCount;
 	int testsPassed = 0;
-	int previousSupplyCount, previousDiscardCount, postSupplyCount, postDiscardCount;
+	int previousSupplyCount, previousDiscardCount, postSupplyCount, postDiscardCount, previousHandCount, postHandCount, previousDeckCount, postDeckCount;
     int k[10] = {adventurer, council_room, feast, gardens, mine
                , remodel, smithy, village, baron, great_hall};
     struct gameState G;
@@ -38,24 +38,48 @@ int main() {
             {
                 memset(&G, 23, sizeof(struct gameState));   // clear the game state
                 r = initializeGame(numPlayer, k, seed, &G); // initialize a new game
-				//supplyCount = sizeOfSupplyCount(&G);
+
+				// Get state property counts prior to gainCard call
 				previousSupplyCount = G.supplyCount[supplyPos];
 				previousDiscardCount = G.discardCount[p];
-                G.handCount[p] = handCount;                 // set the number of cards on hand
+				previousHandCount = G.handCount[p];
+				previousDeckCount = G.deckCount[p];
+                //G.handCount[p] = handCount;                 // set the number of cards on hand
 				gainCard(supplyPos, &G, toFlag, p);
+
+				// Get state property counts after to gainCard call
 				postSupplyCount = G.supplyCount[supplyPos];
 				postDiscardCount = G.discardCount[p];
+				postHandCount = G.handCount[p];
+				postDeckCount = G.deckCount[p];
 				supplyPos++;
+
+				// Check all post counts to ensure they are correct
+				// toFlag = 0 : add to discard
 				if (toFlag == 0) {
 					if ((postDiscardCount == previousDiscardCount + 1) && (postSupplyCount == previousSupplyCount - 1)) {
 						printf("gainCard():  PASS when add to discard performed with player %d\n", p);
 					}
 					else {
 						printf("gainCard():  FAIL when add to discard performed\n");
-						if (postDiscardCount == previousDiscardCount + 1) { printf("\t Discard Count should be %d, and it is %d.\n", previousDiscardCount + 1, postDiscardCount); }
-						if (postSupplyCount == previousSupplyCount - 1) { printf("\t Supply Count should be %d, and it is %d.\n", previousSupplyCount - 1, postSupplyCount); }
+						if (postDiscardCount != previousDiscardCount + 1) { printf("\t Discard Count should be %d, and it is %d.\n", previousDiscardCount + 1, postDiscardCount); }
+						if (postSupplyCount != previousSupplyCount - 1) { printf("\t Supply Count should be %d, and it is %d.\n", previousSupplyCount - 1, postSupplyCount); }
 					}
 				}
+				// toFlag = 1 : add to deck
+				if (toFlag == 1) {
+					if ((postDeckCount == previousDeckCount + 1) && (previousDiscardCount == postDiscardCount) && (postSupplyCount == previousSupplyCount - 1)) {
+						printf("gainCard():  PASS when add to deck performed with player %d\n", p);
+					}
+					else {
+						printf("gainCard():  FAIL when add to deck performed\n");
+						if (postDeckCount != previousDeckCount + 1) { printf("\t Deck Count should be %d, and it is %d.\n", previousDeckCount + 1, postDeckCount); }
+						if (postDiscardCount != previousDiscardCount) { printf("\t Discard Count should be %d, and it is %d.\n", previousDiscardCount, postDiscardCount); }
+						if (postSupplyCount != previousSupplyCount - 1) { printf("\t Supply Count should be %d, and it is %d.\n", previousSupplyCount - 1, postSupplyCount); }
+					}
+				}
+				// toFlag = 2 : add to hand
+
 			}
 	}
 	testsPassed == 0 ? printf("All tests passed!\n") : printf("All tests did not pass!\n");
