0710de977db7e58e0c69d9b5b6ab973fa488af93
Jackie Bradnan <jmcgee@microsoft.com>
2018-02-03 12:06:45 -0800

    mostly done, need to check everything and add some more error cases

--- projects/bradnanj/dominion/unittest4.c
+++ projects/bradnanj/dominion/unittest4.c
@@ -44,10 +44,11 @@ int main() {
 	supplyCountAfterPurchase = state.supplyCount[supplyPos];
 
 	if ( (result == 0) && (buysLeftAfterPurchase == buysLeftBeforePurchase-1) && (supplyCountAfterPurchase == supplyCountBeforePurchase - 1) && (coinsAfterPurchase == coinsBeforePurchase - getCost(supplyPos)) ) {
-		printf("buyCard():  PASS when buying estate card with %d coins and %d buys remaining.\n", p);
+		printf("buyCard():  PASS when buying estate card with %d coins and %d buys remaining.\n", coinsBeforePurchase, buysLeftBeforePurchase);
 	}
 	else {
-		printf("buyCard():  FAIL when buying estate card with %d coins and %d buys remaining.\n", p);
+		testsPassed = -1;
+		printf("buyCard():  FAIL when buying estate card with %d coins and %d buys remaining.\n", coinsBeforePurchase, buysLeftBeforePurchase);
 		if (buysLeftAfterPurchase != buysLeftBeforePurchase + 1) { printf("\t Buys remaining after purchase should be %d, and it is %d.\n", buysLeftBeforePurchase + 1, buysLeftAfterPurchase); }
 		if (supplyCountAfterPurchase != supplyCountBeforePurchase + 1) { printf("\t Supply count after purchase should be %d, and it is %d.\n", supplyCountBeforePurchase + 1, supplyCountAfterPurchase); }
 		if (coinsAfterPurchase != coinsBeforePurchase + getCost(supplyPos)) { printf("\t Coins remaining after purchase should be %d, and it is %d.\n", coinsBeforePurchase + getCost(supplyPos), coinsAfterPurchase); }
@@ -63,10 +64,11 @@ int main() {
 	supplyCountAfterPurchase = state.supplyCount[supplyPos];
 
 	if ((result == 0) && (buysLeftAfterPurchase == buysLeftBeforePurchase - 1) && (supplyCountAfterPurchase == supplyCountBeforePurchase - 1) && (coinsAfterPurchase == coinsBeforePurchase - getCost(supplyPos))) {
-		printf("buyCard():  PASS when buying province card with %d coins and %d buys remaining.\n", p);
+		printf("buyCard():  PASS when buying province card with %d coins and %d buys remaining.\n", coinsBeforePurchase, buysLeftBeforePurchase);
 	}
 	else {
-		printf("buyCard():  FAIL when buying province card with %d coins and %d buys remaining.\n", p);
+		testsPassed = -1;
+		printf("buyCard():  FAIL when buying province card with %d coins and %d buys remaining.\n", coinsBeforePurchase, buysLeftBeforePurchase);
 		if (buysLeftAfterPurchase != buysLeftBeforePurchase + 1) { printf("\t Buys remaining after purchase should be %d, and it is %d.\n", buysLeftBeforePurchase + 1, buysLeftAfterPurchase); }
 		if (supplyCountAfterPurchase != supplyCountBeforePurchase + 1) { printf("\t Supply count after purchase should be %d, and it is %d.\n", supplyCountBeforePurchase + 1, supplyCountAfterPurchase); }
 		if (coinsAfterPurchase != coinsBeforePurchase + getCost(supplyPos)) { printf("\t Coins remaining after purchase should be %d, and it is %d.\n", coinsBeforePurchase + getCost(supplyPos), coinsAfterPurchase); }
@@ -80,21 +82,57 @@ int main() {
 	coinsAfterPurchase = state.coins;
 	buysLeftAfterPurchase = state.numBuys;
 	supplyCountAfterPurchase = state.supplyCount[supplyPos];
-
+	// You should not be able to buy this card, you have no coins remaining
 	if (result == -1) {
-		printf("buyCard():  PASS when attempting to buy adventurer card with insufficient funds (%d coins and %d buys remaining).\n", p);
+		printf("buyCard():  PASS when attempting to buy adventurer card with insufficient funds (%d coins available).\n", coinsBeforePurchase);
 	}
 	else {
-		printf("buyCard():  FAIL when attempting to buy adventurer card with insufficient funds (%d coins and %d buys remaining).\n", p);
+		testsPassed = -1;
+		printf("buyCard():  FAIL when attempting to buy adventurer card with insufficient funds (%d coins available).\n", coinsBeforePurchase);
 		if (buysLeftAfterPurchase != buysLeftBeforePurchase) { printf("\t Buys remaining after failed purchase should be %d, and it is %d.\n", buysLeftBeforePurchase, buysLeftAfterPurchase); }
 		if (supplyCountAfterPurchase != supplyCountBeforePurchase) { printf("\t Supply count after failed purchase should be %d, and it is %d.\n", supplyCountBeforePurchase, supplyCountAfterPurchase); }
 		if (coinsAfterPurchase != coinsBeforePurchase) { printf("\t Coins remaining after failed purchase should be %d, and it is %d.\n", coinsBeforePurchase, coinsAfterPurchase); }
 	}
 
-	// Test buying cards when there are no more of that card left
+	// Reset Game State
+	memset(&state, 23, sizeof(struct gameState));   // clear the game state
+	r = initializeGame(numPlayer, k, seed, &state); // initialize a new game
 
 	// Test buying cards when there are no more buys left
+	supplyPos = 1; // estate
+	state.supplyCount[supplyPos] = 10;
+	state.numBuys = 0;
+	state.coins = 10;
+	coinsBeforePurchase = state.coins;
+	buysLeftBeforePurchase = state.numBuys;
+	supplyCountBeforePurchase = state.supplyCount[supplyPos];
+	result = buyCard(supplyPos, &state);
+	coinsAfterPurchase = state.coins;
+	buysLeftAfterPurchase = state.numBuys;
+	supplyCountAfterPurchase = state.supplyCount[supplyPos];
 
+	// You should not be able to buy this card, you have no buys remaining
+	if (result == -1) {
+		printf("buyCard():  PASS when attempting to buy estate card with no buys remaining (%d buys remaining).\n", buysLeftBeforePurchase);
+	}
+	else {
+		testsPassed = -1;
+		printf("buyCard():  FAIL when attempting to buy estate card with no buys remaining (%d buys remaining).\n", buysLeftBeforePurchase);
+	}
+
+	// Test buying card when there are no more of that card available
+	supplyPos = 1; // estate
+	state.supplyCount[supplyPos] = 0;
+	result = buyCard(supplyPos, &state);
+
+	// You should not be able to buy this card, you have no more of this card remaining
+	if (result == -1) {
+		printf("buyCard():  PASS when attempting to buy estate card with no cards of that type remaining.\n");
+	}
+	else {
+		testsPassed = -1;
+		printf("buyCard():  FAIL when attempting to buy estate card with no cards of that type remaining.\n");
+	}
 
 	testsPassed == 0 ? printf("All tests passed!\n") : printf("All tests did not pass!  Please review the test output for more details.\n");
 	return 0;
