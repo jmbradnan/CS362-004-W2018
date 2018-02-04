61d0a8ba6a0d6819a8d6b9a3e7ae0cf4fa036705
Jackie Bradnan <jmcgee@microsoft.com>
2018-02-03 19:26:59 -0800

    more cases for card 2

--- projects/bradnanj/dominion/cardtest2.c
+++ projects/bradnanj/dominion/cardtest2.c
@@ -23,11 +23,8 @@
 #define TESTCARD "adventurer"
 
 int main() {
-	int newCards = 0;
-	int discarded = 1;
-	int xtraCoins = 0;
-	int shuffledCards = 0;
-
+	int testsPassed = 0;
+	int numberOfDiscards;
 	int i, j, m;
 	int handpos = 0, choice1 = 0, choice2 = 0, choice3 = 0, bonus = 0;
 	int remove1, remove2;
@@ -42,13 +39,83 @@ int main() {
 	// initialize a game state and player cards
 	initializeGame(numPlayers, k, seed, &G);
 
-	printf("----------------- Testing Card: %s ----------------\n", TESTCARD);
-
-	// ----------- TEST 1: choice1 = 1 = +2 cards --------------
 	printf("Testing Adventurer card\n");
+	printf("Test 1: pick up two treasure cards (coppers), discard one card.\n");
 
 	// copy the game state to a test case
 	memcpy(&testG, &G, sizeof(struct gameState));
+
+	numberOfDiscards = 1;
+	oldDiscardCount = testG.discardCount[p];
+	oldHandCount = testG.handCount[p];
+	testG.deck[p][4] = 8;
+	cardEffect(adventurer, choice1, choice2, choice3, &testG, handpos, &bonus);
+	//should result in one discarded card, 2 added treasure cards
+	if (oldDiscardCount + numberOfDiscards == testG.discardCount[p] && oldHandCount + 2 == testG.handCount[p]) {
+		printf("AdventurerCard():  PASS\n");
+	}
+	else {
+		testsPassed = -1;
+		printf("AdventurerCard():  FAIL\n");
+		if (oldDiscardCount + numberOfDiscards != testG.discardCount[p]) {
+			printf("\t discard count = %d, expected to be = %d\n", testG.discardCount[p], oldDiscardCount + numberOfDiscards);
+		}
+		if (oldHandCount + 2 != testG.handCount[p]) {
+			printf("\t hand count = %d, expected to be = %d\n", testG.handCount[p], oldHandCount + 2);
+		}
+	}
+	
+	printf("Test 2: pick up two treasure cards (copper and silver), discard two cards.\n");
+	numberOfDiscards = 2;
+	// copy the game state to a test case
+	memcpy(&testG, &G, sizeof(struct gameState));
+
+	oldDiscardCount = testG.discardCount[p];
+	oldHandCount = testG.handCount[p];
+	testG.deck[p][4] = 8;
+	testG.deck[p][3] = 7;
+	testG.deck[p][1] = 5;
+	cardEffect(adventurer, choice1, choice2, choice3, &testG, handpos, &bonus);
+	//should result in one discarded card, 2 added treasure cards
+	if (oldDiscardCount + numberOfDiscards == testG.discardCount[p] && oldHandCount + 2 == testG.handCount[p]) {
+		printf("AdventurerCard():  PASS\n");
+	}
+	else {
+		testsPassed = -1;
+		printf("AdventurerCard():  FAIL\n");
+		if (oldDiscardCount + numberOfDiscards != testG.discardCount[p]) {
+			printf("\t discard count = %d, expected to be = %d\n", testG.discardCount[p], oldDiscardCount + numberOfDiscards);
+		}
+		if (oldHandCount + 2 != testG.handCount[p]) {
+			printf("\t hand count = %d, expected to be = %d\n", testG.handCount[p], oldHandCount + 2);
+		}
+	}
+
+	printf("Test 3: pick up two treasure cards (silver and gold), discard zero cards.\n");
+	numberOfDiscards = 0;
+	// copy the game state to a test case
+	memcpy(&testG, &G, sizeof(struct gameState));
+
+	oldDiscardCount = testG.discardCount[p];
+	oldHandCount = testG.handCount[p];
+	testG.deck[p][4] = 5;
+	testG.deck[p][3] = 6;
+	
+	cardEffect(adventurer, choice1, choice2, choice3, &testG, handpos, &bonus);
+	//should result in one discarded card, 2 added treasure cards
+	if (oldDiscardCount + numberOfDiscards == testG.discardCount[p] && oldHandCount + 2 == testG.handCount[p]) {
+		printf("AdventurerCard():  PASS\n");
+	}
+	else {
+		testsPassed = -1;
+		printf("AdventurerCard():  FAIL\n");
+		if (oldDiscardCount + numberOfDiscards != testG.discardCount[p]) {
+			printf("\t discard count = %d, expected to be = %d\n", testG.discardCount[p], oldDiscardCount + numberOfDiscards);
+		}
+		if (oldHandCount + 2 != testG.handCount[p]) {
+			printf("\t hand count = %d, expected to be = %d\n", testG.handCount[p], oldHandCount + 2);
+		}
+	}
 
 	/*
 	The following test was designed to make adventurer keep digging for treasure.     It causes an infinite loop so it is not enabled.
@@ -61,19 +128,8 @@ int main() {
 
 	cardEffect(adventurer, choice1, choice2, choice3, &testG, handpos, &bonus);
 	*/
-	oldDiscardCount = testG.discardCount[p];
-	oldHandCount = testG.handCount[p];
-	testG.deck[p][4] = 8;
-	cardEffect(adventurer, choice1, choice2, choice3, &testG, handpos, &bonus);
-	//should result in one discarded card, 2 added treasure cards
-	if (oldDiscardCount + 1 == testG.discardCount[p] && oldHandCount + 2 == testG.handCount[p]) {
-		printf("AdventurerCard():  PASS\n");
-	}
-	else {
-		printf("AdventurerCard():  FAIL\n");
-	}
-	
 
+	testsPassed == 0 ? printf("All tests passed!\n") : printf("All tests did not pass!  Please review the test output for more details.\n");
 
 	return 0;
 }
